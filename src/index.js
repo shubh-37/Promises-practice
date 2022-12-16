a27b25dcc61d10db47ef60ff51d8697471fdff47
Shubh Arya <shubh.arya37@gmail.com>
2022-12-16 10:44:50 +0000

    Update 13 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./styles.css";
+// Task 2: Create a guessing game.
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// User story: User can enter a number
+// User story: The system pick a random number from 1 to 6
+// User story: If user number is equal to a random number, give user 2 points
+// User story: If the user number is different from the random number by 1,
+// give the user 1 point, otherwise, give the user 0 points.
+// User story: User can decide to play the game as long as they want to
+
+
