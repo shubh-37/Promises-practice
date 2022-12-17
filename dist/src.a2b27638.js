b2f559350df1b4933fd0731884242554c6529291
Shubh Arya <shubh.arya37@gmail.com>
2022-12-17 11:17:23 +0000

    Update 75a0ce221da5b9de1b5cb4a55c3ba9.json and src.a2b27638.js

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -259,7 +259,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46367" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35683" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
